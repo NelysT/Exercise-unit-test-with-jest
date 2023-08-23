b26752a2254c15f3d296a92d226694765f5ccfc5
NelysT <139475672+NelysT@users.noreply.github.com>
2023-08-23 22:00:30 +0000

    actualizando en mi rama

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ test("One euro should be 1.206 dollars", function(){
     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
 });
 
-test("One dollar should be 153.48 ", function(){
+test("One dollar should be 153.48 yen", function(){
     // importo la funcion desde app.js
     const { fromDollarToYen }= require('./app.js')
         // hago mi comparacion (la prueba)
